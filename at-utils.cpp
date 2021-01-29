976dcdecae317b8f299bd689b18ccd00c10d5ea8
sinpo828 <sinpo828@126.com>
2021-01-29 20:22:54 +0800

    Update at-utils.cpp
    ignore SIGCHLD

--- at-utils.cpp
+++ at-utils.cpp
@@ -9,6 +9,7 @@
 #include <thread>
 
 #include <getopt.h>
+#include <signal.h>
 
 #include "subjectIMPL.hpp"
 #include "observerIMPL.hpp"
@@ -205,6 +206,7 @@ int main(int argc, char **argv)
     int cid = 1;
     ATCommand *pATCmd = nullptr;
 
+    signal(SIGCHLD, SIG_IGN);
     ON_SCOPE_EXIT
     {
         if (pATCmd)
